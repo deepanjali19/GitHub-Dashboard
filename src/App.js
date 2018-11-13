0b96e6d651aafba24647ae8e5947b5d77a8b62aa
Thanh Nguyen <thanh1094@yahoo.com>
2018-11-13 15:26:15 -0500

    Create mock-up pages using React Router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import './App.css';
-
-import GitHubImage from './img/GitHub-Logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home';
+import CurrentIssues from './CurrentIssues';
+import IssueCommentAmount from './IssueCommentAmount';
+import IssueLabel from './IssueLabel';
+import LanguageList from './LanguageList';
+import PieChart from './PieChart';
+import RelatingComments from './RelatingComments';
+import RepoRecentActivity from './RepoRecentActivity';
+import TotalStars from './TotalStars';
+import UserRecentActivities from './UserRecentActivities';
 
 class App extends Component {
-	
+
 	state = {}
 
-  render() {
-
-    return (
-      <div className="App">
-        <img src={GitHubImage} width="200" height="100" alt="GitHub Logo" />
-        <header className="App-header">
-			   	<h2> GitHub-Dashboard </h2>
-
-			 		<input className="github" type="submit" value="Login to Github" />
-					
-        </header>
-      </div>
-    );
-  }
-
+	render() {
+		return (
+			<Switch>
+				<Route exact path='/' render={() => (
+					<Home />
+				)} />
+				<Route exact path='/CurrentIssues' render={() => (
+					<CurrentIssues />
+				)} />
+				<Route exact path='/IssueCommentAmount' render={() => (
+					<IssueCommentAmount />
+				)} />
+				<Route exact path='/IssueLabel' render={() => (
+					<IssueLabel />
+				)} />
+				<Route exact path='/LanguageList' render={() => (
+					<LanguageList />
+				)} />
+				<Route exact path='/PieChart' render={() => (
+					<PieChart />
+				)} />
+				<Route exact path='/RelatingComments' render={() => (
+					<RelatingComments />
+				)} />
+				<Route exact path='/RepoRecentActivity' render={() => (
+					<RepoRecentActivity />
+				)} />
+				<Route exact path='/TotalStars' render={() => (
+					<TotalStars />
+				)} />
+				<Route exact path='/UserRecentActivities' render={() => (
+					<UserRecentActivities />
+				)} />
+				<Route render={() => (
+						<h1>Not Found</h1>
+				)} />
+			</Switch>
+		);
+	}
 }
 
 export default App;

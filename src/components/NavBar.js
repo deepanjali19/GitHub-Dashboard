47f2111bf4a585540a3a5f9446fcb51aee06c399
dleung25 <dleung25@myseneca.ca>
2018-12-12 18:58:22 -0500

    fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 export const NavBar = () => (
 		<nav className="navbar navbar-inverse">
                     <div className="container-fluid">
